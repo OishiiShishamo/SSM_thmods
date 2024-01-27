882d6acf05f9c48538d18d7dee8bacd305e0935b
Shishamo <shishamodesu@outlook.jp>
2024-01-28 02:51:08 +0900

    aaaa

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "contact": "shishamodesu@outlook.jp",
+    "id": "SSM_thmods",
+    "title": "SSM's Patches",
+    "servers": [
+        "https://mirrors.thpatch.net/SSM/"
+    ],
+    "patches": {
+        "Moon2Earth": "The moon becomes earth."
+    }
+}
