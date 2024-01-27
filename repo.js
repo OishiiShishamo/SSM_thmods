6116113634092db2341b9a40b56f4c7c0840a7c4
Shishamo <shishamodesu@outlook.jp>
2024-01-28 06:51:26 +0900

    UnkoFO

--- repo.js
+++ repo.js
@@ -6,6 +6,7 @@
         "https://mirrors.thpatch.net/SSM/"
     ],
     "patches": {
-        "Moon2Earth": "The moon becomes earth."
+        "Moon2Earth": "The moon becomes earth.",
+        "UnkoFO": "UFO venturers and UFOs will be Unko."
     }
 }
